f27a10b229ada80d22ae568b168cdd03741386ce
Raj Magesh Gauthaman <git@raj-magesh.org>
2026-03-24 15:08:18 -0400

    fix: typing for utility function

--- src/hough_3d_line_detector/_utilities.py
+++ src/hough_3d_line_detector/_utilities.py
@@ -5,7 +5,9 @@ FloatArray = npt.NDArray[np.floating]
 
 
 def convert_parameters_to_coordinates(
-    parameters: FloatArray, *, line: tuple[FloatArray, FloatArray]
+    parameters: FloatArray,
+    *,
+    line: tuple[FloatArray, FloatArray],
 ) -> FloatArray:
     a, b = line
     return a + np.multiply.outer(parameters, b)
@@ -15,13 +17,15 @@ def find_nearest_points_on_line(
     points: FloatArray,
     *,
     line: tuple[FloatArray, FloatArray],
-) -> FloatArray | tuple[FloatArray, FloatArray]:
+) -> FloatArray:
     a, b = line
     return b @ (points - a).T / np.linalg.norm(b)
 
 
 def compute_distances_to_line(
-    points: FloatArray, *, line: tuple[FloatArray, FloatArray]
+    points: FloatArray,
+    *,
+    line: tuple[FloatArray, FloatArray],
 ) -> FloatArray:
     t = find_nearest_points_on_line(points, line=line)
     return np.linalg.norm(
