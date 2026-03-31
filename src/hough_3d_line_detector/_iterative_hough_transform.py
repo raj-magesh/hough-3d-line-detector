fb06dafd21cc2014d3622de787e80a932595eef1
Raj Magesh Gauthaman <git@raj-magesh.org>
2026-03-31 15:30:06 -0400

    feat(doc): add some function hints and a threshold parameter

--- src/hough_3d_line_detector/_iterative_hough_transform.py
+++ src/hough_3d_line_detector/_iterative_hough_transform.py
@@ -9,6 +9,21 @@ from ._utilities import (
 
 
 def _create_roberts_coefficients(directions: FloatArray) -> FloatArray:
+    """Re-parameterize directions using Roberts' optimal line representation.
+
+    Parameters
+    ----------
+    directions:
+        (n_directions, 3) array of unit vectors corresponding to directions in
+        3D space
+
+    Returns
+    -------
+    coefficients
+        (n_directions, 3, 2) array of coefficients used to convert points from
+        (x', y')-space to (x, y, z)-space
+
+    """
     b_x, b_y, b_z = np.unstack(directions, axis=-1)
 
     c = (b_x * b_y) / (1 + b_z)
@@ -30,6 +45,7 @@ class IterativeHoughTransform:
         min_votes: int = 2,
         n_divisions: int = 4,
         n_grid: int = 2**6,
+        threshold: float | None = None,
     ) -> None:
         self.min_votes = min_votes
         self.max_lines = max_lines
@@ -37,6 +53,7 @@ class IterativeHoughTransform:
         self._directions = create_discretized_directions(n_divisions=n_divisions)
         self._coefficients = _create_roberts_coefficients(self._directions)
         self._n_grid = n_grid
+        self._threshold = threshold
 
         self._votes = np.zeros(
             (len(self._coefficients), self._n_grid, self._n_grid),
@@ -59,14 +76,18 @@ class IterativeHoughTransform:
                 np.argmax(self._votes),
                 self._votes.shape,
             )
-            a = np.einsum(
-                "rs,s->r",
-                self._coefficients[i_direction, ...],
-                -self._diagonal / 2
-                + self._diagonal
-                * np.array([i_x_prime, i_y_prime])
-                / (self._n_grid - 1),
+
+            # (x', y') grid parameters:
+            # x' and y' ranges: (-self._diagonal / 2, self._diagonal / 2)
+            # number of intervals: (self._n_grid - 1)
+
+            # get primed coordinates from (x', y') grid indices
+            a_prime = self._diagonal * (
+                np.array([i_x_prime, i_y_prime]) / (self._n_grid - 1) - 0.5
             )
+
+            # apply self._coefficients to a' to get a in (x, y, z)-space
+            a = self._coefficients[i_direction, ...] @ a_prime
             b = self._directions[i_direction]
 
             filter_ = self._filter_for_points_near_line(points=points, line=(a, b))
@@ -81,7 +102,7 @@ class IterativeHoughTransform:
 
             points = points[~filter_]
 
-            self._votes = self._hough_transform(points_on_line, increase_votes=False)
+            self._hough_transform(points_on_line, increase_votes=False)
 
             if len(points_on_line) >= self.min_votes:
                 lines.append((a + self._offset, b))
@@ -99,8 +120,10 @@ class IterativeHoughTransform:
         *,
         increase_votes: bool,
     ) -> npt.NDArray[np.int64]:
-        array = np.einsum("dqr,pq->dpr", self._coefficients, points, optimize=True)
-        indices = np.floor(self._n_grid * (array / self._diagonal + 0.5)).astype(
+        xy_prime = np.einsum("dqr,pq->dpr", self._coefficients, points, optimize=True)
+
+        # get (x', y') grid indices from primed coordinates
+        indices = np.floor(self._n_grid * (xy_prime / self._diagonal + 0.5)).astype(
             np.uint32
         )
 
@@ -110,13 +133,14 @@ class IterativeHoughTransform:
                 1 if increase_votes else -1
             ) * count
 
-        return self._votes
-
     def _center_points(self, points: FloatArray) -> FloatArray:
-        self._x_min = points.min(axis=0)
-        self._x_max = points.max(axis=0)
-        self._diagonal = np.linalg.norm(self._x_max - self._x_min)
-        self._offset = 0.5 * (self._x_min + self._x_max)
+        self._r_min = points.min(axis=0)
+        self._r_max = points.max(axis=0)
+        self._offset = 0.5 * (self._r_min + self._r_max)
+
+        self._diagonal = np.linalg.norm(self._r_max - self._r_min)
+        self._threshold = self._threshold or self._diagonal / self._n_grid
+
         return points - self._offset
 
     def _filter_for_points_near_line(
@@ -126,4 +150,4 @@ class IterativeHoughTransform:
         line: tuple[FloatArray, FloatArray],
     ) -> FloatArray:
         d = compute_distances_to_line(points, line=line)
-        return d < self._diagonal / self._n_grid
+        return d < self._threshold
